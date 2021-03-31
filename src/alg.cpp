fe580b1e759c9200f75c8c0f8acf62931a1c16a5
Arina Rukavishnikova <75798616+rukavishnikovaA@users.noreply.github.com>
2021-03-31 22:44:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,8 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
- int n = 0;
-  for( int i = 0; i < len-1; i++)
-  {
-    for( int j = i + 1; j < len; j++)
-    {
+int n = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int j = i + 1; j < len; j++) {
     if (arr[i] + arr[j] == value)
     n++;
     }
@@ -12,8 +10,8 @@ int countPairs1(int *arr, int len, int value) {
 return n;
 }
 int countPairs2(int *arr, int len, int value) {
- unsigned int n = 0, i = 0, l = 0, r = len - 1;
-    while (l<r) {
+unsigned int n = 0, i = 0, l = 0, r = len - 1;
+    while (l < r) {
         i = 1;
         while (true) {
             if (arr[r] > value || arr[l] + arr[r] > value) {
@@ -32,24 +30,19 @@ int countPairs2(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
 int n = 0;
-  for( int i = 0; i < len - 1; i++)
-  {
+  for (int i = 0; i < len - 1; i++) {
   int left = i, right = len;
-    while (left < right-1)
-    {
+    while (left < right-1) {
     int midle = (left + right) / 2;
-      if (arr[i] + arr[midle] == value)
-      {
+      if (arr[i] + arr[midle] == value) {
       n++;
       int j = midle+1;
-        while (arr[i] + arr[j] == value && j <right)
-        {
+        while (arr[i] + arr[j] == value && j <right) {
         n++;
         j++;
         }
       j = midle - 1;
-          while (arr[i] + arr[j] == value && j>left)
-          {
+          while (arr[i] + arr[j] == value && j > left) {
           n++;
           j--;
           }
